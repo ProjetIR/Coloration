da1f633e13215f25e45c2a35cd5e04e0c67375ea
KIEFFER Emmanuel <kieffer.emmanuel57@sfr.fr>
2013-02-01 16:36:28 +0100

    Début implémentation algorithme

---
+++ src/Algorithm/Agents/MasterController.java
@@ -0,0 +1,12 @@
+package Algorithm.Agents;
+
+import java.awt.Color;
+
+public class MasterController {
+
+	public Color[] getColors() {
+		// TODO Stub de la méthode généré automatiquement
+		return null;
+	}
+
+}
--- src/Algorithm/Agents/VertexController.java
+++ src/Algorithm/Agents/VertexController.java
@@ -2,6 +2,7 @@ package Algorithm.Agents;
 
 import java.awt.Color;
 import java.util.Collection;
+import java.util.HashMap;
 
 import Model.GraphException;
 import Model.Graphe;
@@ -10,18 +11,17 @@ import Model.Vertex;
 public class VertexController extends Thread {
 	
 	private Vertex v;
-	private Graphe graphe;
 	private Collection<Vertex> neighbours;
-	private int conflicts;
+	private MasterController master;
 	
-	public VertexController(Vertex v, Graphe graphe) {
+	public VertexController(Vertex v,Collection<Vertex> neighbours,MasterController master) {
 		super();
 		try {
 		
 			this.v = v;
-			this.graphe = graphe;
-			neighbours = graphe.getNeighbours(v);
-			conflicts = computeConflictsNumber();
+			this.neighbours = neighbours;
+			this.master = master;
+			
 			
 		} catch (GraphException e) {
 			// TODO Bloc catch généré automatiquement
@@ -32,31 +32,55 @@ public class VertexController extends Thread {
 	@Override
 	public void run() {
 		// TODO Stub de la méthode généré automatiquement
-		while(!this.isInterrupted()){
-			Color minimiseConflicts;
-			for(Vertex vertex : neighbours){
+		try {
+			while(!this.isInterrupted()){
 				
-				synchronized (vertex) {
-					
-					if(vertex.getInfo().getCol() == v.getInfo().getCol()){
-						
+
+				
+				
+			}
+		} catch (InterruptedException e) {
+			// TODO Bloc catch généré automatiquement
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
+	private int[] getRepartionColor(Color[] availableColor,Collection<Vertex> neighbours ) throws InterruptedException{
+		
+		int[] repartition = new int[availableColor.length];
+		for(int i = 0;i<availableColor.length;i++){
+			for(Vertex v : neighbours){
+				synchronized (v) {
+					if(availableColor[i] == v.getInfo().getCol()){
+						repartition[i]++;
 					}
 				}
 			}
-			
+		//System.out.println("Thread vertex "+this.v.getId()+" s'endort");
+		//Thread.sleep(10);
+		//System.out.println("Thread vertex "+this.v.getId()+" s'endort");
 		}
+		return repartition;
 	}
 	
-	private int computeConflictsNumber(){
-		int nb = 0;
-		for(Vertex vertex : neighbours){
-			if(v.getInfo().getCol() == v.getInfo().getCol()){
-				nb++;
+	private int[] sommeAndargMIN(int[] tab){
+		int min = Integer.MAX_VALUE;
+		int argmin = 0;
+		int somme = 0;
+		int[] values = new int[2];
+		for(int i = 0 ;i <tab.length;i++){
+			if(tab[i]<min){
+				argmin = i;
+				min = tab[i];
 			}
+			somme+=tab[i];
 		}
-		return nb;
+		values[0] = somme;
+		values[1] = argmin;
+		return values;
 	}
 	
 	
-
 }
